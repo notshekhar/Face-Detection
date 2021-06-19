2c46ab023a6b8d86898443068a0ff850a78cea95
Shekhar Tyagi <notshekhar@gmail.com>
2021-06-19 18:50:42 +0530

    new

--- js/index.js
+++ js/index.js
@@ -36,5 +36,7 @@ video.addEventListener("play", () => {
         )
         canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height)
         faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpression(canvas, resizedDetections)
     }, 100)
 })
